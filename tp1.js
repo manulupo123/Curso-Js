26d0100e5d1c516e091f3ced185b4c365a76964e
manu <manu.lupo@hotmail.com>
2023-12-04 20:25:45 -0300

    se agrega practico 1

---
+++ tp1.js
@@ -0,0 +1,18 @@
+//ejercicio 1
+let nombre="Facundo"
+let apellido= "Porra"
+let edad= "28"
+console.log("Hola,",nombre,apellido,".Tienes",edad, "años")
+
+//ejercicio2
+let persona= {nombre:"manuel",edad:"28",apellido:"pepelino"}
+console.log("mi objeto persona es",JSON.stringify(persona))
+
+//ejercicio 3
+let num1=5;num2=10;num3=15; num4=20
+console.log(num1+num2)
+let num5=15
+console.log(num4-num3)
+let num6=5
+console.log(num5*num6)
+let resultadofinal=44
